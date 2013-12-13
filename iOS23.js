41d04e53ab9e4bd156f52324dfa2cef6ed5a2711
Omar Sainz <SQL_Slammer.exe@live.de>
2013-12-13 14:38:22 -0700

    Un archivo más. ~
    console.log("Hola Comunidad");

---
+++ iOS23.js
@@ -0,0 +1,14 @@
+Propósitos del 2014
+
+Hola viteh. 
+
+Mis propósitos para el próximo año, son los siguientes... 
+
+- Especializarme en UX Design. :D 
+- Mejorar mi portafolio personal.
+- Ayudar a mis compañeros en lo que pueda. 
+- Ser más social que José. :D 
+- Aprender y sacarle el máximo provecho a la Comunidad de Platzi. (:
+
+
+Omar Sainz
