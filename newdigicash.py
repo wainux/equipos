29a1cd49732a1d13d89f1dd94db960643f486bba
Edwin Sigcha <newdigicash@gmail.com>
2013-12-13 16:40:49 -0500

    Create newdigicash.py
    propósitos del 2014

---
+++ newdigicash.py
@@ -0,0 +1,9 @@
+## Propósito del 2014
+
+Hola que tal!!
+
+El próximo año yo ayudaré a difundir python-django y nodejs en Ecuador, y trabajaré creando mi portafolio.
+
+Seguiré estudiando con los cursos nuevos de @mejorandola y completaré los cursos que aún tengo pendiente.
+
+Edwin Sigcha
